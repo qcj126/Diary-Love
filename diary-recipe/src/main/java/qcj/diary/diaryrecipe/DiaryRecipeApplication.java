6322393e8d915b4ff0ec2d3beeeaaad3a78f39c9
Chase <1262901257@qq.com>
2026-04-30 16:48:51 +0800

    新增食谱功能

---
+++ diary-recipe/src/main/java/qcj/diary/diaryrecipe/DiaryRecipeApplication.java
@@ -0,0 +1,13 @@
+package qcj.diary.diaryrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryRecipeApplication.class, args);
+    }
+
+}
