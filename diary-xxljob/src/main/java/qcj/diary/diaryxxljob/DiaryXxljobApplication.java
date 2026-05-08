1c574b94846dad192592470c4166efdea8d3f24e
Chase <1262901257@qq.com>
2026-05-08 18:01:48 +0800

    定时任务框架

---
+++ diary-xxljob/src/main/java/qcj/diary/diaryxxljob/DiaryXxljobApplication.java
@@ -0,0 +1,13 @@
+package qcj.diary.diaryxxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryXxljobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryXxljobApplication.class, args);
+    }
+
+}
